cfd742c5616ed40c7a0243bee7ac45b005dcbd92
freelife1191 <freelife1191.good@gmail.com>
2025-08-21 21:23:54 +0900

    Part 4. NestJS 핵심개념 - Ch4. 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService], // 인스턴스화 해서 컨트롤러에 주입
+  imports: [MovieModule],
 })
 export class AppModule {}
