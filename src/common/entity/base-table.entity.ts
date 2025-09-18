62828eab92fc09bc79b40db3312c4339ddf970b7
freelife1191 <freelife1191.good@gmail.com>
2025-09-18 17:43:57 +0900

    Part 11. Serialization

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,12 +1,17 @@
+import { Exclude, Expose } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude() // 응답 데이터에 제외
+  @Expose() // 응답 데이터에 포함
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
