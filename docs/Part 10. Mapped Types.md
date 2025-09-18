49d6993093565a8badb32f67efceaaa581919a6d
freelife1191 <freelife1191.good@gmail.com>
2025-09-18 17:38:25 +0900

    Part 10. Mapped Types

---
+++ docs/Part 10. Mapped Types.md
@@ -0,0 +1,5 @@
+# Part 10. Mapped Types
+
+---
+
+https://codefactory.notion.site/Mapped-Types-4e40fcc2e1554c8f901a097b64bfd522
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,38 +1,4 @@
-import {
-  ArrayNotEmpty,
-  IsArray,
-  IsDefined,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  registerDecorator,
-  ValidationArguments,
-  ValidationOptions,
-  ValidatorConstraintInterface,
-} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto {
-  @IsNotEmpty()
-  @IsString()
-  @IsOptional()
-  title?: string;
-
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsNumber(
-    {}, // 첫 번째 인자는 숫자 타입의 고유의 옵션
-    { each: true } // 배열의 모든 요소가 숫자인지 검사
-  )
-  @IsOptional()
-  genreIds?: number[];
-
-  @IsNotEmpty()
-  @IsOptional()
-  detail?: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  @IsOptional()
-  directorId?: number;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
